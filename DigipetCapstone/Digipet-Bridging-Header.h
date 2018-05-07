7bb5f2b36055700014bc5231a9d430048ca8a5bf
Katie <katkatz@bergen.org>
2018-05-07 08:49:19 -0400

    header is NECESSARY

---
+++ DigipetCapstone/Digipet-Bridging-Header.h
@@ -0,0 +1,40 @@
+//
+//  Digipet-Bridging-Header.h
+//  Digipet
+//
+//  Created by Katie Katz on 3/26/18.
+//  Copyright © 2018 Katie Katz. All rights reserved.
+//
+
+#ifndef Digipet_Bridging_Header_h
+#define Digipet_Bridging_Header_h
+
+#import "RZTransitionsNavigationController.h"
+#import "RZTransitionAction.h"
+#import "RZUniqueTransition.h"
+#import "RZBaseSwipeInteractionController.h"
+#import "RZHorizontalInteractionController.h"
+#import "RZOverscrollInteractionController.h"
+#import "RZPinchInteractionController.h"
+#import "RZTransitionInteractionControllerProtocol.h"
+#import "RZTransitionsInteractionControllers.h"
+#import "RZVerticalSwipeInteractionController.h"
+#import "RZTransitionsManager.h"
+#import "RZTransitions.h"
+#import "RZAnimationControllerProtocol.h"
+#import "RZCardSlideAnimationController.h"
+#import "RZCirclePushAnimationController.h"
+#import "RZRectZoomAnimationController.h"
+#import "RZSegmentControlMoveFadeAnimationController.h"
+#import "RZShrinkZoomAnimationController.h"
+#import "RZTransitionsAnimationControllers.h"
+#import "RZZoomAlphaAnimationController.h"
+#import "RZZoomBlurAnimationController.h"
+#import "RZZoomPushAnimationController.h"
+#import "NSObject+RZTransitionsViewHelpers.h"
+#import "UIImage+RZTransitionsFastImageBlur.h"
+#import "UIImage+RZTransitionsSnapshotHelpers.h"
+
+
+
+#endif /* Digipet_Bridging_Header_h */
