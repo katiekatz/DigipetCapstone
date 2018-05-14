231f12ed93daa9e00d5aa77d147c3f38f0e222f7
Emma Ingallinera <einge113@gmail.com>
2018-05-14 08:28:31 -0400

    wig

---
+++ DigipetCapstone/Play.swift
@@ -0,0 +1,104 @@
+//
+//  Play.swift
+//  DigipetCapstone
+//
+//  Created by Emma Ingallinera on 5/14/18.
+//  Copyright © 2018 Katie Katz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Play : UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var level1: UIButton!
+    @IBOutlet weak var level2: UIButton!
+    @IBOutlet weak var level3: UIButton!
+    @IBOutlet weak var level4: UIButton!
+    @IBOutlet weak var level5: UIButton!
+    @IBOutlet weak var level6: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.searchBar.endEditing(true)
+    }
+    
+    @IBAction func backButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func levelOneTouched(_ sender: Any) {
+        self.transitioningDelegate = RZTransitionsManager.shared()
+        let nextViewController = storyboard?.instantiateViewController(withIdentifier: "levelVC")
+        (nextViewController as! LevelVC).text = "1"
+        nextViewController?.transitioningDelegate = RZTransitionsManager.shared()
+        self.present(nextViewController!, animated:true) {}
+    }
+    
+    @IBAction func levelTwoTouched(_ sender: Any) {
+        self.transitioningDelegate = RZTransitionsManager.shared()
+        let nextViewController = storyboard?.instantiateViewController(withIdentifier: "levelVC")
+        (nextViewController as! LevelVC).text = "2"
+        nextViewController?.transitioningDelegate = RZTransitionsManager.shared()
+        self.present(nextViewController!, animated:true) {}
+    }
+    
+    @IBAction func levelThreeTouched(_ sender: Any) {
+        self.transitioningDelegate = RZTransitionsManager.shared()
+        let nextViewController = storyboard?.instantiateViewController(withIdentifier: "levelVC")
+        (nextViewController as! LevelVC).text = "3"
+        nextViewController?.transitioningDelegate = RZTransitionsManager.shared()
+        self.present(nextViewController!, animated:true) {}
+    }
+    
+    @IBAction func levelFourTouched(_ sender: Any) {
+        self.transitioningDelegate = RZTransitionsManager.shared()
+        let nextViewController = storyboard?.instantiateViewController(withIdentifier: "levelVC")
+        (nextViewController as! LevelVC).text = "4"
+        nextViewController?.transitioningDelegate = RZTransitionsManager.shared()
+        self.present(nextViewController!, animated:true) {}
+    }
+    
+    @IBAction func levelFiveTouched(_ sender: Any) {
+        self.transitioningDelegate = RZTransitionsManager.shared()
+        let nextViewController = storyboard?.instantiateViewController(withIdentifier: "levelVC")
+        (nextViewController as! LevelVC).text = "5"
+        nextViewController?.transitioningDelegate = RZTransitionsManager.shared()
+        self.present(nextViewController!, animated:true) {}
+    }
+    
+    @IBAction func levelSixTouched(_ sender: Any) {
+        self.transitioningDelegate = RZTransitionsManager.shared()
+        let nextViewController = storyboard?.instantiateViewController(withIdentifier: "levelVC")
+        (nextViewController as! LevelVC).text = "6"
+        nextViewController?.transitioningDelegate = RZTransitionsManager.shared()
+        self.present(nextViewController!, animated:true) {}
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
